e759f8eb3284434da8c3229f227da4a502f94c00
Kyle Mathews <mathews.kyle@gmail.com>
2017-01-23 18:15:58 -0800

    Tighten up typography

--- utils/typography.js
+++ utils/typography.js
@@ -1,5 +1,4 @@
 const Typography = require('typography')
-const React = require('react')
 const theme = require('typography-theme-de-young').default
 const CodePlugin = require('typography-plugin-code').default
 
@@ -16,8 +15,11 @@ const CodePlugin = require('typography-plugin-code').default
 theme.plugins = [
   new CodePlugin(),
 ]
-theme.baseLineHeight = 1.6
+theme.baseLineHeight = 1.45
 theme.overrideThemeStyles = () => ({
+  'h1,h2,h3,h4,h5,h6': {
+    lineHeight: 1,
+  },
   'tt,code': {
     fontSize: '70%',
   },
@@ -25,6 +27,7 @@ theme.overrideThemeStyles = () => ({
     lineHeight: 1.22,
   },
 })
+
 //theme = require('typography-theme-moraga').default
 //theme['@media only screen and (max-width: 768px)'].blockquote.marginLeft = "-21px"
 
@@ -32,7 +35,6 @@ const typography = new Typography(theme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
-  const ReactDOM = require('react-dom/server')
   typography.injectStyles()
 }
 
