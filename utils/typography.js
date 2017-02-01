a5912b3fcf833b953cffd433a1406fb665fa811e
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-01 12:16:07 -0800

    Moved some of my theme tweaks upstream

--- utils/typography.js
+++ utils/typography.js
@@ -1,44 +1,21 @@
 const Typography = require('typography')
-const theme = require('typography-theme-de-young').default
+let theme = require('typography-theme-de-young').default
 const CodePlugin = require('typography-plugin-code').default
 
 theme.plugins = [
   new CodePlugin(),
 ]
-theme.baseLineHeight = 1.43
+theme.baseLineHeight = 1.4
 theme.blockMarginBottom = 0.75
 theme.overrideThemeStyles = ({ rhythm }) => ({
-  h1: {
-    color: `hsla(0,0%,0%,0.75)`,
-  },
-  h2: {
-    color: `hsla(0,0%,0%,0.775)`,
-  },
-  h3: {
-    color: `hsla(0,0%,0%,0.85)`,
-  },
-  'h1,h2,h3,h4,h5,h6': {
-    lineHeight: 1,
-  },
-  'h1,h2,h3,h4': {
-    lineHeight: 1,
-    marginTop: rhythm(1),
-    marginBottom: rhythm(1/2),
-  },
-  'h4,h5,h6': {
-    textTransformation: `uppercase`,
-  },
   'blockquote > h1, blockquote > h2, blockquote > h3, blockquote > h4': {
     marginTop: 0,
-  },
-  ul: {
-    marginTop: rhythm(1/2),
   },
   'li > p': {
     marginBottom: rhythm(1/2),
   },
   'p code': {
-    fontSize: '78%',
+    fontSize: '75%',
   },
   'tt,code': {
     fontSize: '85%',
@@ -48,7 +25,24 @@ theme.overrideThemeStyles = ({ rhythm }) => ({
   },
 })
 
+//theme = require('typography-theme-doelger').default
 const typography = new Typography(theme)
+
+//import ReactDOM from 'react-dom/server'
+//import React from 'react'
+//import { GoogleFont } from 'react-typography'
+
+//// Hot reload typography in development.
+//if (process.env.NODE_ENV !== 'production') {
+  //typography.injectStyles()
+  //if (typeof document !== 'undefined') {
+    //const googleFonts = ReactDOM.renderToStaticMarkup(
+      //React.createFactory(GoogleFont)({ typography })
+    //)
+    //const head = document.getElementsByTagName('head')[0]
+    //head.insertAdjacentHTML('beforeend', googleFonts)
+  //}
+//}
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
