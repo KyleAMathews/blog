e4e52ebe699675580ff802a35d207b5522cce492
Kyle Mathews <mathews.kyle@gmail.com>
2017-02-18 20:05:15 -0800

    Test again

--- pages/template-app-shell.js
+++ pages/template-app-shell.js
@@ -4,7 +4,7 @@ import browserHistory from 'react-router/lib/browserHistory'
 class AppShell extends React.Component {
   componentDidMount () {
     console.log(this.props.location)
-    //browserHistory.replace(this.props.location.pathname)
+    browserHistory.replace(this.props.location.pathname)
   }
 
   render () {
