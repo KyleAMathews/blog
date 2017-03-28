7d63bf25a65ae4ccf2fb7566c015c775eca3951c
Kyle Mathews <mathews.kyle@gmail.com>
2017-03-28 10:27:29 -0400

    update to latest canary and add 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class FourOFour extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Page not found</h1>
+      </div>
+    );
+  }
+}
+
+export default FourOFour;
