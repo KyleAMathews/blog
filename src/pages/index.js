5bbce026ddba35c070a23133e74d93b4e4277dfe
Kyle Mathews <mathews.kyle@gmail.com>
2017-07-06 15:32:29 -0700

    Upgrade to 1.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,9 +73,9 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(
-      limit: 2000,
-      sort: { fields: [frontmatter___date], order: DESC },
-      filter: { frontmatter: { draft: { ne: true } } },
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { draft: { ne: true } } }
     ) {
       edges {
         node {
