d2c3c0e577b3135f0a9cbf89b97b71ca0dff7a31
Kyle Mathews <mathews.kyle@gmail.com>
2018-08-31 19:09:17 -0700

    Cleanup typography file

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -23,24 +23,7 @@ theme.overrideThemeStyles = ({ rhythm }) => ({
   },
 })
 
-//theme = require('typography-theme-doelger').default
 const typography = new Typography(theme)
-
-//import ReactDOM from 'react-dom/server'
-//import React from 'react'
-//import { GoogleFont } from 'react-typography'
-
-//// Hot reload typography in development.
-//if (process.env.NODE_ENV !== 'production') {
-//typography.injectStyles()
-//if (typeof document !== 'undefined') {
-//const googleFonts = ReactDOM.renderToStaticMarkup(
-//React.createFactory(GoogleFont)({ typography })
-//)
-//const head = document.getElementsByTagName('head')[0]
-//head.insertAdjacentHTML('beforeend', googleFonts)
-//}
-//}
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== "production") {
