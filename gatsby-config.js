5631b003e4fb97a2d0bc21f0e69883d0c7c6d211
Bryan Braun <bbraun7@gmail.com>
2023-11-03 18:57:30 -0400

    fix: ensure RSS feed is linked consistently (#83)
    fixes KyleAMathews/blog#31

--- gatsby-config.js
+++ gatsby-config.js
@@ -119,14 +119,7 @@ module.exports = {
               }
             `,
             output: "/rss.xml",
-            title: "Your Site's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/blog/",
-            // optional configuration to specify external rss feed, such as feedburner
-            link: "https://feeds.feedburner.com/gatsby/blog",
+            title: "Bricolage",
           },
         ],
       },
